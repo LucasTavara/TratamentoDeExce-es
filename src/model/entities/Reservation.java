bcdc9bbd5d04f91edaf6358e9d0807c059799556
LucasTavares <lulucas25@hotmail.com>
2024-07-30 01:55:59 -0300

    Good solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,9 +1,9 @@
 package model.entities;
 
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
+import model.exceotions.DomainException;
 
 public class Reservation {
 
@@ -12,58 +12,54 @@ public class Reservation {
 	private Date checkOut;
 
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	
+
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("check-out date must be after check-in date");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 	}
+
 	public Integer getRoomNumber() {
 		return roomNumber;
 	}
+
 	public void setRoomNumber(Integer roomNumber) {
 		this.roomNumber = roomNumber;
 	}
+
 	public Date getCheckIn() {
 		return checkIn;
 	}
+
 	public Date getCheckOut() {
 		return checkOut;
-	}	
-	public long duration() {
-		long diff = checkOut.getTime()-checkIn.getTime();
-		return TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);
 	}
-	public String updateDates(Date checkIn, Date checkOut) {
-		
+
+	public long duration() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+
+	public void updateDates(Date checkIn, Date checkOut) {
+
 		Date now = new Date();
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return "Reservation dates for update must be future dates";
-		} 
-		if (!checkOut.after(checkIn)) {
-			return "check-out date must be after check-in date";
+			throw new DomainException("Reservation dates for update must be future dates");
 		}
-		
+
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;
+
 	}
-	
-@Override
-public String toString() {
-	return "Room "
-			+roomNumber
-			+", check-in: "
-			+sdf.format(checkIn)
-			+", check-out: "
-			+sdf.format(checkOut)
-			+", "
-			+duration()
-			+ " nigths";
-			
-}
-	
-	
-	
+
+	@Override
+	public String toString() {
+		return "Room " + roomNumber + ", check-in: " + sdf.format(checkIn) + ", check-out: " + sdf.format(checkOut)
+				+ ", " + duration() + " nigths";
+
+	}
 
 }
---
+++ src/model/exceotions/DomainException.java
@@ -0,0 +1,9 @@
+package model.exceotions;
+
+public class DomainException extends RuntimeException{
+	private static final long serialVersionUID = -1L;
+
+	public DomainException(String msg) {
+		super(msg);
+	}
+}
